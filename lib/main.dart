f98bc17f7a6afee27352b252f809884c05555ae1
TayyabaIshfaq <79797290+TayyabaIshfaq947@users.noreply.github.com>
2021-03-09 03:49:15 +0500

    Update main.dart
    Multi-Child widget
    2:Row widget

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,51 @@ class MyApp extends StatelessWidget {
               Container(
                 width: double.infinity,
               ),
+               Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(13.0),
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 26.0),
+            //Row widegt
+                 child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.greenAccent,
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+923007721118',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(13.0),
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 26.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.greenAccent,
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'taibatahira84@gmail.com',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
