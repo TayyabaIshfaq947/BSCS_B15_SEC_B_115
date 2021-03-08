67583dcc1f204c0eb63e86332c756a3384fd1c5f
TayyabaIshfaq <79797290+TayyabaIshfaq947@users.noreply.github.com>
2021-03-09 02:22:03 +0500

    First part of My card app
    using material app old method creating app and add some color

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    // Old method of code
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent,
+        body: Container(),
+      ),
+    ),
+  );
+}
